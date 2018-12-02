659036b276dd046a2a3707dc6c154e7585ed7f24
Wes Payne <noblepayne@noblepayne.com>
2018-12-01 22:39:30 -0800

    day2

--- src/advent_of_code2018/d2p0.cljs
+++ src/advent_of_code2018/d2p0.cljs
@@ -5,4 +5,29 @@
   (->> input
        str/split-lines))
 
-(defn solve [input] 0)
+(defn count-same-letters [string]
+  (let [freq (frequencies string)
+        lcountset (set (map val freq))]
+    {:twos   (if (lcountset 2)
+              1
+              0)
+     :threes (if (lcountset 3)
+              1
+              0)}))
+
+(defn process-lines [lines]
+  (reduce
+    (fn [totalcount line]
+      (let [linecount (count-same-letters line)]
+        (merge-with + totalcount linecount)))
+    {:twos 0 :threes 0}
+    lines))
+
+(defn checksum [{:keys [:twos :threes]}]
+  (* twos threes))
+
+(defn solve [input]
+  (-> input
+      parse-input
+      process-lines
+      checksum))
--- src/advent_of_code2018/d2p1.cljs
+++ src/advent_of_code2018/d2p1.cljs
@@ -1,5 +1,28 @@
 (ns advent-of-code2018.d2p1
   (:require [clojure.string :as str]
-            [advent-of-code2018.d2p0 :as d2p0]))
+            [advent-of-code2018.d2p0 :as d2p0]
+            [clojure.math.combinatorics :as combo]))
 
-(defn solve [input] 1)
+(defn string-match? [[s1 s2]]
+  (let [wrong-count (->> (map not= s1 s2)
+                         (filter identity)
+                         (count))]
+    (if (= 1 wrong-count)
+      true
+      false)))
+
+(defn find-boxes [box-combos]
+  (filter string-match? box-combos))
+
+(defn find-common-letters [[[box1 box2]]]
+  (->> (map vector box1 box2)
+       (filter #(apply = %))
+       (map first)
+       (apply str)))
+
+(defn solve [input]
+  (-> input
+      d2p0/parse-input
+      (combo/combinations 2)
+      find-boxes
+      find-common-letters))
